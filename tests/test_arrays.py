4a4493e48e3b645fd577d010ae3837d9ea9f03a7
rodrigo <barbaresco.rodrigo@gmail.com>
2018-06-13 16:49:58 -0300

    Adding more one test to arrays

--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -21,6 +21,10 @@ class ArraysTest(unittest.TestCase):
         array = [5, 4, 3, 1, 2]
         self.assertEqual(3, arrays.middle(array))
 
+    def test_should_sum_the_elements_of_an_array(self):
+        array = [5, 4, 3, 1, 2]
+        self.assertEqual(15, arrays.sum(array))
+
     def test_should_count_how_many_occurences_of_a_element(self):
         array = [5, 4, 3, 1, 2, 2]
         self.assertEqual(2, arrays.count(array, 2))
